fc4c4e606ea37e2d76fd4ada7cfeed220340a5f9
Gabriele Monaco <gmonaco@redhat.com>
2026-01-05 14:34:15 +0100

    rv: Add KUnit stubs for current and smp_processor_id()
    Some monitors do not only rely on tracepoint arguments but also on the
    currently executing task and current processor.
    This makes it more challenging to mock events in KUnit.
    
    Define wrapper functions around current and smp_processor_id(), the
    functionality is stubbed only during KUnit, however the additional
    function call is necessary whenever the KUnit tests are built in.

--- include/rv/ltl_monitor.h
+++ include/rv/ltl_monitor.h
@@ -9,6 +9,7 @@
 #include <linux/stringify.h>
 #include <linux/seq_buf.h>
 #include <rv/instrumentation.h>
+#include <rv/kunit_stubs.h>
 #include <trace/events/task.h>
 #include <trace/events/sched.h>
 
